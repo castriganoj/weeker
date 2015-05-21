10b580a4c2c7aadd8e72183362ed70a1a10efd45
Jim C <castriganoj@gmail.com>
2015-05-21 21:24:46 +0000

    installed and set up rspec w/ capabarya

---
+++ spec/controllers/goal_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+   describe GoalsController do
+
+
+  it "should redirect to the current week's page" do
+    get 'root'
+    this_week = Date.today.strftime("%U").to_i
+    response.should redirect_to(lists_path/this_week)
+  end
+end
+  
